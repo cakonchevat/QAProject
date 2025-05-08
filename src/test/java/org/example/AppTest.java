f10002072fdfc7cf2ba290bcd39eaa3896edf141
cakonchevat <139081286+cakonchevat@users.noreply.github.com>
2025-05-08 08:35:00 +0200

    Initial commit

---
+++ src/test/java/org/example/AppTest.java
@@ -0,0 +1,11 @@
+package org.example;
+
+import org.testng.annotations.Test;
+
+public class AppTest {
+
+    @Test
+    public void sampleTest() {
+        System.out.println("Appium test goes here!");
+    }
+}
